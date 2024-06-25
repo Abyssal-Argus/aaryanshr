852c966b37e9ed14e286812baa38ac49e6f98b4a
Aaryan Raj Shrestha <115132304+Abyssal-Argus@users.noreply.github.com>
2024-06-25 20:23:57 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark': '#1B1B1B',
+        'foot-black':'#2d2e32',
+        'foot-dark': '#111111',
+        'custom-blue':'#87CEFA',
+        'custom-red':'#E60026',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
